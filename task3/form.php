2f3dec79d995c7e3f0aeda377ef557a5178515bb
Arslanov Suleiman <103919339+arslanov-sea@users.noreply.github.com>
2024-03-18 15:16:55 +0300

    Update form.php

--- task3/form.php
+++ task3/form.php
@@ -1,39 +1,39 @@
-<form action="" method="POST"> 
- <label>Введите имя <br/> 
-<input type="text" placeholder="Введите свое имя" name="thename"> 
-</label><br> 
-<label>Введите свой телефон<br/> 
-<input type="tel" placeholder="Введите свой телефон" name="thephone"> 
-</label><br> 
-<label> Введите свой e-mail<br/> 
-<input type="email" placeholder="Введите e-mail" name="youremail"></label><br/> 
-<label>Введите дату рождения<br/> 
-<input type="data" placeholder="Введите дату рождения" name="givebirth"></label><br/> 
-<label>Выберите пол<br/> 
-<input type="radio" name="sex" value="female">Женский<br/> 
-<input type="radio" name="sex" value="male">Мужской<br/></label><br/> 
+<form action="" method="POST">
+  <label for="fio">ФИО:</label>
+  <input type="text" id="fio" name="fio" required /><br>
+
+  <label for="tel">Телефон:</label>
+  <input type="tel" id="tel" name="tel" pattern="[0-9]{10}" required /><br>
+
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email" required /><br>
 <label>Выберите ЯП<br/></label> 
 <select multiple="multiple" name="choosing"> 
 <option>C++</option> 
 <option>Pascal</option> 
 <option>JavaScript</option> 
 </select></br> 
-<label>Биография<br/> 
-<textarea placeholder="Напишите биографию" name="biography"></textarea> 
-</label></br> 
-<label>Чекбокс</label><br/> 
-<input type="checkbox" name="checkboxx"><label> С контрактом ознакомлен(а) 
-</label><br/> 
-<button>Сохранить</button> 
-  <input name="fio" /> 
-  <select name="year"> 
-    <?php  
-    for ($i = 1922; $i <= 2022; $i++) { 
-      printf('<option value="%d">%d год</option>', $i, $i); 
-    } 
-    ?> 
-  </select> 
- 
-   
-  <input type="submit" value="ok" /> 
+
+  
+  <label>Пол:</label>
+  <input type="radio" id="male" name="gender" value="male">
+  <label for="male">Мужской</label>
+  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">Женский</label><br>
+
+  <label for="bio">О себе:</label>
+  <textarea id="bio" name="bio"></textarea><br>
+
+  <input type="checkbox" id="contract" name="contract" value="1" required>
+  <label for="contract">С контрактом ознакомлен(а)</label><br>
+
+  <select id="year" name="year">
+    <?php 
+    for ($i = 1922; $i <= 2022; $i++) {
+      printf('<option value="%d">%d год</option>', $i, $i);
+    }
+    ?>
+  </select><br>
+
+  <button type="submit">Сохранить</button>
 </form>
